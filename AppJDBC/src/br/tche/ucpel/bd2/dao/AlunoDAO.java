85f99145ec3a7a2c7d498fa49b422cccd7f92ac7
LeoMarotta <sen.aka@hotmail.com>
2023-12-05 09:08:28 -0300

    AppSwing, JDBC e criação de arquivos.

---
+++ AppJDBC/src/br/tche/ucpel/bd2/dao/AlunoDAO.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.tche.ucpel.bd2.dao;
+
+/**
+ *
+ * @author leomarotta
+ */
+public class AlunoDAO {
+    
+}
---
+++ CRUD_JDBC/src/main/java/treino/crud_jdbc/CRUD_JDBC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package treino.crud_jdbc;
+
+/**
+ *
+ * @author leomarotta
+ */
+public class CRUD_JDBC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
